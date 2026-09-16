71872576c9d7efd73069585372d2d93593be90b5
Aditya <adityamondal.cps@gmail.com>
2026-09-16 22:26:04 +0530

    Test timetable day selection

--- VIT-OS/test/widget_test.dart
+++ VIT-OS/test/widget_test.dart
@@ -6,7 +6,20 @@ void main() {
   testWidgets('shows the CampusOS timetable', (tester) async {
     await tester.pumpWidget(const ProviderScope(child: CampusOsApp()));
     await tester.pumpAndSettle();
+
     expect(find.text('CampusOS'), findsOneWidget);
+    expect(find.text('Monday'), findsOneWidget);
     expect(find.text('Data Structures and Algorithms'), findsOneWidget);
+  });
+
+  testWidgets('switches timetable day', (tester) async {
+    await tester.pumpWidget(const ProviderScope(child: CampusOsApp()));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Tue'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Tuesday'), findsOneWidget);
+    expect(find.text('No classes scheduled for this day.'), findsOneWidget);
   });
 }
